042272409bdc64e73bf755d04cd6c56e9a43cee5
Kirill Zverev <vesnanervakh@gmail.com>
2025-12-12 18:52:13 +0300

    fix: BackdropBlur overlay above all elements when it should not

--- app/lib/common/ui/backdrop-blur.tsx
+++ app/lib/common/ui/backdrop-blur.tsx
@@ -1,13 +1,20 @@
 const backdropBlurElemId = "backdrop-blur";
 
 export default function BackdropBlur() {
-    return <div id={backdropBlurElemId} className={"fixed top-0 left-0 h-screen w-screen z-1 transition-opacity opacity-0 backdrop-blur-xs"}></div>
+    return <div id={backdropBlurElemId} className={"hidden fixed top-0 left-0 h-screen w-screen z-1 transition-opacity opacity-0 backdrop-blur-xs"}></div>
 }
 
 export function enableBackDropBlur() {
-    document.getElementById(backdropBlurElemId)?.classList.add("opacity-100");
+    document.getElementById(backdropBlurElemId)?.classList.remove("hidden");
+    setTimeout(() => {
+        document.getElementById(backdropBlurElemId)?.classList.add("opacity-100");
+    }, 50)
+
 }
 
 export function disableBackDropBlur() {
     document.getElementById(backdropBlurElemId)?.classList.remove("opacity-100");
+    setTimeout(() => {
+        document.getElementById(backdropBlurElemId)?.classList.add("hidden");
+    }, 50)
 }
